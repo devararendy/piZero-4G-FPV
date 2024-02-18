c84d15676fa9cb595dba3d2ddcf7ae8420d5aa45
Rendy <rendy_devara@msn.com>
2024-02-19 00:36:34 +0700

    added binary mavp2p and the service

--- systemd-service/install-services.sh
+++ systemd-service/install-services.sh
@@ -3,23 +3,29 @@
 echo "disable if exist"
 sudo systemctl disable zerotier.service
 sudo systemctl disable fpvstream.service
+sudo systemctl disable mavlink.service
 
 echo "installing Services"
 
 sudo cp ./zerotier.service  /lib/systemd/system/zerotier.service
 sudo cp ./fpvstream.service /lib/systemd/system/fpvstream.service
+sudo cp ./mavlink.service /lib/systemd/system/mavlink.service
+
 sudo systemctl daemon-reload
 
 echo "enabling Services..."
 sudo systemctl enable zerotier.service
 sudo systemctl enable fpvstream.service
+sudo systemctl enable mavlink.service
 
 echo "starting Services..."
 sudo systemctl start zerotier.service
 sudo systemctl start fpvstream.service
+sudo systemctl start mavlink.service
 
 echo "geting Status..."
 sudo systemctl status zerotier.service
 sudo systemctl status fpvstream.service
+sudo systemctl status mavlink.service
 
 echo "Done..."
